7064fec299a056fd8fa96c79d7e65eca11b68e9b
dtgraham9 <dtgraham9@gmail.com>
2018-09-20 21:30:05 -0500

    file chooser

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,13 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import javax.swing.JFileChooser; 
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
- * @author graham
+ * @author Graham Thompson
+ * @version
  */
 public class Recursion {
+    public static double harmonic(int harmonic){
+        if(harmonic==0){
+            return 0;
+        }
+        else{
+            return 1/(double) harmonic + harmonic(harmonic-1);
+        }
+    }
     
+    public static void fecthDataFromPath(){
+        
+        try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    }catch(Exception ex) {
+        ex.printStackTrace();
+    }      
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+        "JPG & GIF Images", "jpg", "gif");
+        fileChooser.setFileFilter(filter);
+        fileChooser.showOpenDialog(null);
+        
+//new java.awt.FileDialog((java.awt.Frame) null).setVisible(true);
+    }
+    
+    public static void isabel(int[] A, int[] B){
+        
+    }
 }
